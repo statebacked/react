6ca53c0895602b03e661d5d74704be04f9cfce7b
Adam Berger <adam@simplystated.dev>
2023-09-09 17:36:44 -0400

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import {
+  StateBackedClient,
+  StateValue,
+  Event,
+  Actor,
+  errors,
+} from "@statebacked/client";
+import { useEffect, useState } from "react";
+
+export const useStateBackedMachine = <
+  TEvent extends Exclude<Event, string>,
+  TState extends StateValue,
+  TContext extends Record<string, unknown> & {
+    public?: Record<string, unknown>;
+  },
+>(
+  client: StateBackedClient,
+  {
+    machineName,
+    instanceName,
+    machineVersionId,
+    getInitialContext,
+    onEventSendingError,
+  }: {
+    machineName: string;
+    instanceName: string;
+    machineVersionId?: string;
+    getInitialContext: () => Partial<TContext>;
+    onEventSendingError?: (event: TEvent, error: Error) => void;
+  },
+) => {
+  const [actor, setActor] = useState<Actor<
+    TEvent,
+    TState,
+    TContext["public"]
+  > | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [inFlightEvents, setInFlightEvents] = useState(
+    new Set<Parameters<(typeof actor)["send"]>[0]>(),
+  );
+
+  useEffect(() => {
+    setActor(null);
+    let sub = null;
+
+    client.machineInstances
+      .getOrCreateActor<TEvent, TState, TContext>(
+        machineName,
+        instanceName,
+        () => ({
+          context: getInitialContext(),
+          machineVersionId,
+        }),
+      )
+      .then((actor) => {
+        sub = actor.subscribe(
+          () => {
+            setInFlightEvents(new Set(actor.inFlightEvents));
+            setError(null);
+          },
+          (error) => {
+            if (error instanceof errors.ActorEventSendingError) {
+              onEventSendingError?.(error.event, error);
+            }
+            setError(error);
+          },
+        );
+        const origSend = actor.send;
+        actor.send = function (event) {
+          origSend.call(this, event);
+          setInFlightEvents(new Set(actor.inFlightEvents));
+        };
+        setActor(actor);
+      });
+
+    return () => {
+      if (sub) {
+        sub.unsubscribe();
+      }
+    };
+  }, [client, machineName, instanceName, machineVersionId]);
+
+  return {
+    actor,
+    isLoading: actor === null,
+    error,
+    inFlightEvents,
+    areEventsInFlight: inFlightEvents.size > 0,
+  };
+};
